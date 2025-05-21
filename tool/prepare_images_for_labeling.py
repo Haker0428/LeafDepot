558365a01112f7634d3d4a725a23873474d1447d
Haker0428 <513193084@qq.com>
2025-05-22 00:48:09 +0800

    add image part labels

--- tool/prepare_images_for_labeling.py
+++ tool/prepare_images_for_labeling.py
@@ -9,7 +9,7 @@ os.makedirs(output_dir, exist_ok=True)
 for idx, fname in enumerate(sorted(os.listdir(input_dir))):
     if fname.lower().endswith(('.jpg', '.jpeg', '.png')):
         img = Image.open(os.path.join(input_dir, fname)).convert('RGB')
-        img = img.resize((640, 640))  # YOLO推荐输入尺寸
+        img = img.resize((1280, 1280))  # YOLO推荐输入尺寸
         new_name = f"img{idx:05d}.jpg"
         img.save(os.path.join(output_dir, new_name))
 
