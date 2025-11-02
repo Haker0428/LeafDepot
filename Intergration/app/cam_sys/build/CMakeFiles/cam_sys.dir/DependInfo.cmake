dd5b9e05bf53bfd26a441bf997511160f9ffa2ac
big box <big box@qq.com>
2025-11-02 22:32:54 +0800

    修复图片保存bug

---
+++ Intergration/app/cam_sys/build/CMakeFiles/cam_sys.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/LeafDepot/Intergration/app/cam_sys/src/CameraController.cpp" "CMakeFiles/cam_sys.dir/src/CameraController.cpp.o" "gcc" "CMakeFiles/cam_sys.dir/src/CameraController.cpp.o.d"
+  "/home/ubuntu/LeafDepot/Intergration/app/cam_sys/src/main.cpp" "CMakeFiles/cam_sys.dir/src/main.cpp.o" "gcc" "CMakeFiles/cam_sys.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Intergration/app/cam_sys/build/CMakeFiles/cam_sys.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam_sys.dir/src/CameraController.cpp.o"
+  "CMakeFiles/cam_sys.dir/src/CameraController.cpp.o.d"
+  "CMakeFiles/cam_sys.dir/src/main.cpp.o"
+  "CMakeFiles/cam_sys.dir/src/main.cpp.o.d"
+  "cam_sys"
+  "cam_sys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam_sys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
